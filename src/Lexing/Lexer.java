441b1058d87d4cba54d0c8a8d138238a46042283
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-12-29 14:34:29 +0200

    FIX error in Lexer
    Fix error in Lexer with incorrect substring of original line for blockwords with args.
    
    Signed-off-by: Stanislav <56927835+Stanislavius@users.noreply.github.com>

--- src/Lexing/Lexer.java
+++ src/Lexing/Lexer.java
@@ -187,9 +187,8 @@ public class Lexer {
                 if (st.substring(0, word.length() + 1).equals(word + " ")) {
                     int start = indentation * 4;
                     tokens.add(new Token(TokenType.BLOCKWORD, word, lineNum, start+1, originalString));
-                    st = st.substring(0, start) +
-                            " ".repeat(word.length()) +
-                            st.substring(start+word.length());
+                    st = " ".repeat(word.length()) +
+                            st.substring(word.length());
                 }
             }
         }
