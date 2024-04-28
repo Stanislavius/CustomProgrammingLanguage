08f2dabceb211765d1556efa296560ac23b1870d
Stanislav <stanislavtotkal@gmail.com>
2024-04-28 21:33:51 +0300

    DEV tab removing
    Signed-off-by: Stanislav <stanislavtotkal@gmail.com>

--- src/IDE/MainWindow.java
+++ src/IDE/MainWindow.java
@@ -3,22 +3,27 @@ package IDE;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.text.Element;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MainWindow extends JFrame implements ActionListener {
     private JTextPane pane;
     JTextArea lines;
+    JLabel label;
     public MainWindow() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //add(scrollPane);
         pane = new JTextPane();
+        setLayout(new BorderLayout());
         JScrollPane scrollPane = new JScrollPane();
         lines = new JTextArea("1");
         lines.setBackground(Color.LIGHT_GRAY);
         lines.setEditable(false);
+        label = new JLabel("Some text");
         pane.getDocument().addDocumentListener(new DocumentListener() {
             public String getText() {
                 int caretPosition = pane.getDocument().getLength();
@@ -43,14 +48,52 @@ public class MainWindow extends JFrame implements ActionListener {
             }
         });
         //scrollPane.add(pane);
-        add(scrollPane);
+        add(scrollPane, BorderLayout.CENTER);
         scrollPane.getViewport().add(pane);
         scrollPane.setRowHeaderView(lines);
         //this.add(area);
+        add(label, BorderLayout.SOUTH);
         this.pack();
+        ((AbstractDocument)this.pane.getDocument()).setDocumentFilter(new DocumentFilter(){
+            public void remove(DocumentFilter.FilterBypass fb, int offset, int length) {
+                try {
+                    if (length == 1 && offset >= 3) {
+                        if (pane.getDocument().getText(offset - 3, 4).equals(" ".repeat(4))) {
+                            fb.remove(offset - 3, 4);
+                        } else
+                            fb.remove(offset, length);
+
+                    } else
+                        fb.remove(offset, length);
+                }
+                catch (BadLocationException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        //StyledDocument doc = new DefaultStyledDocument();
+        //doc.
+        //pane = new JTextPane()
+        pane.addKeyListener(new KeyAdapter()
+        {
+            public void keyPressed(KeyEvent e)
+            {
+                if (e.getKeyCode() == KeyEvent.VK_TAB)
+                {
+                    e.consume();
+                    try {
+                        pane.getDocument().insertString(pane.getCaretPosition(), " ".repeat(4), null);
+                    } catch (BadLocationException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
         this.setSize(new Dimension(500, 500));
         this.repaint();
         this.revalidate();
+
 
     }
     @Override
