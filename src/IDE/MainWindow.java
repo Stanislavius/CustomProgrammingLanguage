1845c29adead16578155dc1db55eca8d5a22f439
Stanislav <stanislavtotkal@gmail.com>
2024-04-29 16:36:04 +0300

    Dev syntax highlight
    Signed-off-by: Stanislav <stanislavtotkal@gmail.com>

--- src/IDE/MainWindow.java
+++ src/IDE/MainWindow.java
@@ -1,5 +1,10 @@
 package IDE;
 
+import Lexing.Exceptions.LexingException;
+import Lexing.Lexer;
+import Lexing.Token;
+import Parsing.Parser;
+
 import javax.swing.*;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
@@ -11,13 +16,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainWindow extends JFrame implements ActionListener {
     private JTextPane pane;
     JTextArea lines;
     JLabel label;
+    Lexer lexer = new Lexer();
+    Parser parser = new Parser();
+
     public MainWindow() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         //add(scrollPane);
         pane = new JTextPane();
         setLayout(new BorderLayout());
@@ -48,7 +61,70 @@ public class MainWindow extends JFrame implements ActionListener {
             public void removeUpdate(DocumentEvent de) {
                 lines.setText(getText());
             }
+
         });
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r);
+            }
+        });
+        executor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                lexer.clear();
+                LinkedList<Token> tokens = lexer.read(pane.getText());
+                Style defaultStyle = StyleContext.
+                        getDefaultStyleContext().
+                        getStyle(StyleContext.DEFAULT_STYLE);
+                pane.getStyledDocument().setCharacterAttributes(0, pane.getStyledDocument().getLength(), defaultStyle, true);
+
+                String keywords = "\\belif|while|if|class|def|try|else|\\b";
+                Pattern compiledPattern = Pattern.compile(keywords);
+                Matcher matcher = null;
+                try {
+                    matcher = compiledPattern.matcher(pane.getDocument().getText(0, pane.getDocument().getLength()));
+                } catch (BadLocationException e) {
+                    throw new RuntimeException(e);
+                }
+                while (matcher.find()){
+                    System.out.println("found");
+                    AttributeSet attr = new SimpleAttributeSet();
+                    StyledDocument style = pane.getStyledDocument();
+                    AttributeSet oldSet = style.getCharacterElement(0).getAttributes();
+                    StyleContext sc = StyleContext.getDefaultStyleContext();
+                    AttributeSet s = sc.addAttribute(oldSet, StyleConstants.Foreground, Color.ORANGE);
+                    style.setCharacterAttributes(matcher.start(), matcher.end() - matcher.start(), s, true);
+                    pane.repaint();
+                    System.out.println(pane.getText());
+                    System.out.println(pane.getText().length());
+                }
+
+                if (lexer.isWithoutError()) {
+                    parser.clear();
+                    parser.parse(tokens);
+                }
+                else{
+                    LinkedList<LexingException> exceptions = lexer.getExceptions();
+                    StyledDocument style = pane.getStyledDocument();
+                    Element root = pane.getDocument().getDefaultRootElement();
+                    for (LexingException exception: exceptions){
+                        int lineNum = exception.getLineNum();
+                        int position = exception.getPosition();
+                        int offset = root.getElement( lineNum - 1 ).getStartOffset() + position;
+                        System.out.println(lineNum + " " + position + " " + offset);
+                        AttributeSet oldSet = style.getCharacterElement(0).getAttributes();
+                        StyleContext sc = StyleContext.getDefaultStyleContext();
+                        AttributeSet s = sc.addAttribute(oldSet, StyleConstants.Background, Color.RED);
+                        style.setCharacterAttributes(offset, 1, s, true);
+                        pane.repaint();
+                    }
+                }
+            }
+        }, 1, 2, TimeUnit.SECONDS);
+
+
         //scrollPane.add(pane);
         add(scrollPane, BorderLayout.CENTER);
         scrollPane.getViewport().add(pane);
@@ -72,6 +148,7 @@ public class MainWindow extends JFrame implements ActionListener {
                     ex.printStackTrace();
                 }
             }
+
         });
 
         pane.addCaretListener(new CaretListener() {
@@ -116,14 +193,40 @@ public class MainWindow extends JFrame implements ActionListener {
                 }
             }
         });
+
+
         this.setSize(new Dimension(500, 500));
         this.repaint();
         this.revalidate();
 
-
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public int getLineNum(int caretPos){
+        int rowNum = (caretPos == 0) ? 1 : 0;
+        for (int offset = caretPos; offset > 0;) {
+            try {
+                offset = Utilities.getRowStart(pane, offset) - 1;
+            } catch (BadLocationException ex) {
+                throw new RuntimeException(ex);
+            }
+            rowNum++;
+        }
+        return rowNum;
+    }
+
+    public int getColNum(int caretPos){
+        int offset = 0;
+        try {
+            offset = Utilities.getRowStart(pane, caretPos);
+        } catch (BadLocationException ex) {
+            throw new RuntimeException(ex);
+        }
+        int colNum = caretPos - offset + 1;
+        return colNum;
     }
 }
